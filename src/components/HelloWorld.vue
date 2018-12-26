b134710a61932b1174232b9ebd9d992de8b323c1
Tanay Vardhan <tvardha2@illinois.edu>
2018-12-26 06:40:01 +0530

    basic socket.io application

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,59 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="card mt-3">
+      <div class="card-body">
+          <div class="card-title">
+              <h3>Chat Group</h3>
+              <hr>
+          </div>
+          <div class="card-body">
+              <div class="messages" v-for="(msg, index) in messages" :key="index">
+                  <p><span class="font-weight-bold">{{ msg.user }}: </span>{{ msg.message }}</p>
+              </div>
+          </div>
+      </div>
+      <div class="card-footer">
+          <form @submit.prevent="sendMessage">
+              <div class="gorm-group">
+                  <label for="user">User:</label>
+                  <input type="text" v-model="user" class="form-control">
+              </div>
+              <div class="gorm-group pb-3">
+                  <label for="message">Message:</label>
+                  <input type="text" v-model="message" class="form-control">
+              </div>
+              <button type="submit" class="btn btn-success">Send</button>
+          </form>
+      </div>
   </div>
 </template>
 
 <script>
+import io from 'socket.io-client';
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    data() {
+        return {
+            user: '',
+            message: '',
+            messages: [],
+            socket : io('localhost:3001')
+        }
+    },
+    methods: {
+        sendMessage(e) {
+            e.preventDefault();
+            
+            this.socket.emit('SEND_MESSAGE', {
+                user: this.user,
+                message: this.message
+            });
+            this.message = ''
+        }
+    },
+    mounted() {
+        this.socket.on('MESSAGE', (data) => {
+            this.messages = [...this.messages, data];
+            // you can also do this.messages.push(data)
+        });
+    }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
